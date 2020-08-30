95cf3fa1cd4c9a3bd6ce407ea271f053b7ba9416
Evan Jacobs <probablyup@gmail.com>
2020-08-30 18:17:43 -0400

    handle keyframes

--- src/stylis-rtl.js
+++ src/stylis-rtl.js
@@ -1,16 +1,16 @@
 // @flow
 
-import cssjanus from "cssjanus";
-import { compile, RULESET, stringify } from 'stylis';
+import cssjanus from 'cssjanus';
+import { compile, KEYFRAMES, RULESET, stringify } from 'stylis';
 
 function stylisRTLPlugin(element: Object, index: number, children: Object[], callback: Function): ?string {
-  if (element.type === RULESET) {
-    element.children = compile(`{${cssjanus.transform(element.children.map(x => x.value).join('\n'))}}`)[0].children
+  if (element.type === RULESET || element.type === KEYFRAMES) {
+    element.children = compile(`{${cssjanus.transform(element.children.map((x) => x.value).join('\n'))}}`)[0].children;
   }
 }
 
 // stable identifier that will not be dropped by minification unless the whole module
 // is unused
-Object.defineProperty(stylisRTLPlugin, "name", { value: "stylisRTLPlugin" });
+Object.defineProperty(stylisRTLPlugin, 'name', { value: 'stylisRTLPlugin' });
 
 export default stylisRTLPlugin;
--- src/stylis-rtl.test.js
+++ src/stylis-rtl.test.js
@@ -1,11 +1,12 @@
 // @flow
-import { compile, middleware, serialize, stringify } from 'stylis';
-import stylisRtlPlugin from './stylis-rtl';
+import { compile, middleware, serialize, stringify } from "stylis";
+import stylisRtlPlugin from "./stylis-rtl";
 
-const stylis = (css) => serialize(compile(css), middleware([stylisRtlPlugin, stringify]));
+const stylis = (css) =>
+  serialize(compile(css), middleware([stylisRtlPlugin, stringify]));
 
-describe('integration test with stylis', () => {
-  it('flips simple rules', () => {
+describe("integration test with stylis", () => {
+  it("flips simple rules", () => {
     expect(
       stylis(
         `.a {
@@ -15,10 +16,12 @@ describe('integration test with stylis', () => {
         }
       `
       )
-    ).toMatchInlineSnapshot(`".a{padding-right:5px;margin-left:5px;border-right:1px solid red;}"`);
+    ).toMatchInlineSnapshot(
+      `".a{padding-right:5px;margin-left:5px;border-right:1px solid red;}"`
+    );
   });
 
-  it('flips shorthands', () => {
+  it("flips shorthands", () => {
     expect(
       stylis(
         `.a {
@@ -30,7 +33,7 @@ describe('integration test with stylis', () => {
     ).toMatchInlineSnapshot(`".a{padding:0 0 0 5px;margin:0 5px 0 0;}"`);
   });
 
-  it('handles noflip directives', () => {
+  it("handles noflip directives", () => {
     expect(
       stylis(
         `
@@ -42,5 +45,37 @@ describe('integration test with stylis', () => {
         `
       )
     ).toMatchInlineSnapshot(`".a{padding:0 5px 0 0;margin:0 5px 0 0;}"`);
+  });
+
+  it("flips keyframes", () => {
+    expect(
+      stylis(
+        `@keyframes a {
+          padding-left: 5px;
+          margin-right: 5px;
+          border-left: 1px solid red;
+        }
+      `
+      )
+    ).toMatchInlineSnapshot(
+      `"@keyframes a{padding-right:5px;margin-left:5px;border-right:1px solid red;}"`
+    );
+  });
+
+  it("flips media queries", () => {
+    expect(
+      stylis(
+        `@media (min-width: 500px) {
+          .a {
+            padding-left: 5px;
+            margin-right: 5px;
+            border-left: 1px solid red;
+          }
+        }
+      `
+      )
+    ).toMatchInlineSnapshot(
+      `"@media (min-width: 500px){.a{padding-right:5px;margin-left:5px;border-right:1px solid red;}}"`
+    );
   });
 });
